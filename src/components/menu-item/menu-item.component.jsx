f28527311cc743ee841027a5d5ba131d83ef490e
Atharva Pardeshi <atharva.exe@gmail.com>
2022-08-25 23:42:44 +0530

    feat: receive title from props and render it

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
+const MenuItem = (props) => (
   <div className="menu-item">
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{props.title}</h1>
       <span className="title">SHOP NOW</span>
     </div>
   </div>
