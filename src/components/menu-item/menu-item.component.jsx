e3e4c09439e7a2c5720f375ea69c7cebb128f537
Atharva Pardeshi <atharva.exe@gmail.com>
2022-08-31 12:27:56 +0530

    feat: make title uppercase

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,7 +7,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     backgroundImage: `url(${imageUrl})`
   }} className={`${size} menu-item`}>
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
