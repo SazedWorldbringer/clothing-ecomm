80e28dc6806c95d54aafd204aa7e97d955b5f775
Atharva Pardeshi <atharva.exe@gmail.com>
2022-09-04 17:50:47 +0530

    fix: typo

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -52,7 +52,7 @@ class Directory extends React.Component {
   render() {
     return(
       <div className="directory-menu">
-        {this.state.sections.map(({ id, {...otherSectionProps} }) => (
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
             <MenuItem key={id} {...otherSectionProps} />
          ))}
       </div>
