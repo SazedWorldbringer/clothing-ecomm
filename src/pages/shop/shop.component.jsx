0e1970544ae77c8e1ce928dbc7b358c3d25ffc97
Atharva Pardeshi <atharva.exe@gmail.com>
2022-09-05 17:05:48 +0530

    feat: import collection-preview and render it

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
@@ -11,7 +13,16 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return <div>SHOP PAGE</div>;
+    const {collections} = this.state;
+    return (
+      <div className="shop-page">
+        {
+          collections.map(({id, ...otherCollectionProps}) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+          ))
+        }
+      </div>
+    )
   }
 }
 
