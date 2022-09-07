619a9c76bdde4854f74b0693d3fa23c75b85efa9
Atharva Pardeshi <atharva.exe@gmail.com>
2022-09-07 17:11:44 +0530

    fix: fix image links

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -7,13 +7,7 @@ const SHOP_DATA = [
       {
         id: 1,
         name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZHW3VTp/brown-brim.png',
-        price: 25,
-      },
-      {
-        id: 1,
-        name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZHW3VTp/brown-brim.png',
+        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
         price: 25,
       },
       {
@@ -135,7 +129,7 @@ const SHOP_DATA = [
       {
         id: 19,
         name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6v0/blue-jean-jacket.png',
+        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
         price: 90,
       },
       {
