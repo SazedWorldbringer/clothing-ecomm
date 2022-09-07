a20b87aec91c2714ad309dde504be4852eab6e26
Atharva Pardeshi <atharva.exe@gmail.com>
2022-09-07 16:58:27 +0530

    fix: add imageUrl to backgroundImage

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -7,7 +7,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div
       className="image"
       style={{
-        backgroundImage: `url(${})`
+        backgroundImage: `url(${imageUrl})`
       }}
     />
     <div className="collection-footer">
