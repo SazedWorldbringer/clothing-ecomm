1e146ae90f5ef4211497b478802d1c6f291b4beb
Atharva Pardeshi <atharva.exe@gmail.com>
2022-09-18 10:01:55 +0530

    feat: write styles for FormInput component and export it

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 import './form-input.styles.scss'
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <div className=""group>
+  <div className="group">
     <input className="form-input" onChange={handleChange} {...otherProps}/>
-    {
-      label ?
-      (<label className={`${props.value.length ? 'shrink' : ''} form-input-label`}>
+    {label ? (
+      <label 
+        className={`${
+          otherProps.value.length ? "shrink" : ""
+        } form-input-label`}
+      >
         {label}
-      </label>)
-      : null
-    }
+      </label>
+    ) : null}
   </div>
 )
+
+export default FormInput;
